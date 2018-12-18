396fd65399e55e5a00a6a62907b10853b49e2be3
qtxz2000 <wenkaizhijia@163.com>
2018-12-18 12:05:25 +0800

    chore: 升级 typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 const gulp = require('gulp')
 const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
+const ts = require('gulp-typescript')
+
+const tsProject = ts.createProject('./tsconfig.json')
 
 gulp.task('build', function(cb) {
-    gulp.src('src/*.*')
-    .pipe(babel({
-      presets: ['env', 'es2015', 'stage-0']
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'))
-    cb()
+    // gulp.src('src/*.*')
+    tsProject
+      .src()
+      .pipe(tsProject())
+      .pipe(babel({
+        presets: ['env', 'es2015', 'stage-0']
+      }))
+      .pipe(uglify())
+      .pipe(gulp.dest('dist'))
+      cb()
 })
 
 gulp.task('default', ['build'])
